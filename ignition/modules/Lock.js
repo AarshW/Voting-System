c59d4dd56f356bbb4e6f00a28def4a3e5dfd3ce3
Aarsh <aarsh0707@gmail.com>
2024-06-22 03:39:26 +0530

    smart contract created and deployed

--- ignition/modules/Lock.js
+++ ignition/modules/Lock.js
@@ -7,9 +7,10 @@ module.exports = buildModule("LockModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const lock = m.contract("Create");
+   
 
   return { lock };
+
+  
 });
